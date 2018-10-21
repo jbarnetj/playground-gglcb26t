916f48a034ffbd40bb05f269471a9ed1913ae855
Victor Lohézic <v.lohezic@laposte.net>
2018-10-21 12:03:16 +0200

    Delete coffre4.py

--- python-project/coffre4.py
+++
@@ -1,7 +0,0 @@
-from random import randint
-
-# la combinaison est un nombre entre 0 et 9.
-combinaison = randint(100, 999)
-
-# Affichage de la combinaison
-print("Bienvenue au CTF (Coffre-Trop-Fort). Confiez-moi tous vos biens!")
